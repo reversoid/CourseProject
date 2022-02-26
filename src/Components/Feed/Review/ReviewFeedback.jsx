8c73d2331f770677c9b2bf8cb4667e958ca66584
George <ancient.gekob@gmail.com>
2022-02-26 19:22:41 +0300

    добавил свертку развертку комментариев

--- src/Components/Feed/Review/ReviewFeedback.jsx
+++ src/Components/Feed/Review/ReviewFeedback.jsx
@@ -1,14 +1,20 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Comment } from "./Comment"
 
 export const ReviewFeedback = () => {
+    let [commentCollapsed, setCommentCollapsed] = useState(true)
+    function toggleComment(){
+        setCommentCollapsed(!commentCollapsed)
+        console.log(commentCollapsed)
+    }
     return (
         <>
             <div className="feedback-panel">
                 <div className="comments-panel">
                     <span className='fw-bold ms-5'>Comments&nbsp;</span>
                     <span>(10)</span>
-                    <div className="down-arrow ms-3"></div>
+                    <div className={commentCollapsed?"down-arrow ms-3":"down-arrow ms-3 rotated"}
+                    onClick={()=>toggleComment()}></div>
                 </div>
 
                 <div className="like-panel">
@@ -16,7 +22,8 @@ export const ReviewFeedback = () => {
                     <div className="like me-5"></div>
                 </div>
             </div>
-            <div className="comments-container container px-5">
+            
+            <div className={commentCollapsed?"container px-5 collapsed":"container px-5"}>
                 <Comment/>
                 <Comment/>
             </div>
