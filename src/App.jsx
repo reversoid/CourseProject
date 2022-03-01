0861a0181c3e382cd36dde6ea9e20e739cd08a15
George <ancient.gekob@gmail.com>
2022-03-01 22:29:44 +0300

    добавил роуты для теста

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Feed } from './Components/Feed/Page/Feed.jsx';
 import { Profile } from './Components/Profile/Profile.jsx';
-
+import {Route, Routes, Link, Navigate} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <Feed/>
+      <Routes> 
+        <Route path='/feed' element={<Feed/>}/>
+        <Route path='*' element={<Navigate to='/feed'/>}/>
+      </Routes>
     </div>
   );
 }
