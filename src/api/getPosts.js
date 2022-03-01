b8334d4ca7d1e4165f12559300b990f77be87371
George <ancient.gekob@gmail.com>
2022-03-01 21:55:48 +0300

    добавил пустое тело запроса в getPosts

--- src/api/getPosts.js
+++ src/api/getPosts.js
@@ -3,7 +3,7 @@ import config from '../config.json'
 
 export const getPosts = async () => {
     try {
-        const response = await axios.post(config.myUrl+'/api/auth/getPosts')
+        const response = await axios.post(config.myUrl+'/api/auth/getPosts', {})
         
         console.log(response.data.posts)
         return response.data.posts
