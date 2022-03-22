22d1fc7e02a5eb3874ce7de8cd874904353f6acd
George <ancient.gekob@gmail.com>
2022-03-22 23:42:15 +0300

    добавил мобильный выбор фильтров

--- src/Components/Feed/Page/Feed.jsx
+++ src/Components/Feed/Page/Feed.jsx
@@ -7,7 +7,7 @@ import { AddReviewModal } from './AddReviewModal'
 import { DropZone } from './DropZone'
 import { getPosts } from '../../../api/getPosts'
 import { getCurrentUserData } from '../../../api/getCurrentUserData'
-import {Navigate} from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 
 export const Feed = (props) => {
 
@@ -16,19 +16,21 @@ export const Feed = (props) => {
     const [id, setId] = useState(1)
     let [filters, setFilters] = useState(new Object())
     // at the start we get posts and current user if logged in
-    useEffect(()=>{
-        getCurrentUserData().then((response)=>
-        {
-            if (!response) {return}
+    useEffect(() => {
+        getCurrentUserData().then((response) => {
+            if (!response) { return }
             setId(response.id)
         })
-        getPosts(filters).then((response)=>{setPosts(response)})
+        getPosts(filters).then((response) => { setPosts(response) })
     }, [])
 
     // when the filter value is changed we get posts with this filter
-    useEffect(()=>{
-        getPosts(filters).then((response)=>{setPosts(response)})
+    useEffect(() => {
+        getPosts(filters).then((response) => { setPosts(response) })
     }, [filters])
+
+    let [mobileFilters, setMobileFilters] = useState('d-none')
+    let [mobileFiltersArrow, setMobileFiltersArrow] = useState('')
 
     let [navigate, setNavigate] = useState('')
     return (
@@ -39,15 +41,36 @@ export const Feed = (props) => {
                 <div className="toolbar container-fluid">
                     <h1 className='py-3 w-100'>Reviews</h1>
                     <div className="add-btn ms-3" data-bs-toggle="modal" data-bs-target="#addModal"></div>
-                    <AddReviewModal/>
+                    <AddReviewModal />
+                </div>
+                <div className="container-fluid mb-3 d-flex align-items-start flex-column d-lg-none">
+                    <div className='d-flex justify-content-center'>
+                        <h3 className='d-inline-block text-start mb-3'>Filters</h3>
+                        <div className={"down-arrow ms-3 "+mobileFiltersArrow} onClick={()=>{
+                            if (mobileFilters){
+                                setMobileFilters('')
+                                setMobileFiltersArrow('rotated')
+                            }
+                            else{
+                                setMobileFilters('d-none')
+                                setMobileFiltersArrow('')
+
+                            }
+                        }}></div>
+                    </div>
+                    
+                    
+                    <div className={"d-block d-lg-none col-6 mx-auto col-md-6 col-12 "+mobileFilters}>
+                        <Filter filters={filters} setFilters={setFilters} />
+                    </div>
                 </div>
 
                 <div className="row">
                     <div className="col-md-12 col-lg-9">
-                        {posts?posts.map((post, index) => {return <Review post={post} key={index} currentId={id}/>}):''}                        
+                        {posts ? posts.map((post, index) => { return <Review post={post} key={index} currentId={id} /> }) : ''}
                     </div>
                     <div className="col-lg-3 d-none d-lg-block">
-                        <Filter filters={filters} setFilters={setFilters}/>
+                        <Filter filters={filters} setFilters={setFilters} />
                     </div>
                 </div>
             </section>
